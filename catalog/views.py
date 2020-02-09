07a7f4df6ffd4d2adb4c03e1c950116fc6c6d9ba
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-09 14:09:27 +0000

    modified files in project folder

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+
 from .models import Course
+from .forms import CourseForm
 
 # Create your views here.
 def show_courses(request):
-    all_courses = Course.object.fliter()
+    all_courses = Course.objects.all()
     return render(request, 'catalog/courses.template.html', {
         'all_courses':all_courses
+    })
+    
+    
+    
+def create_course(request):
+   create_course_form = CourseForm()
+   return render(request, 'catalog/create_course.template.html', {
+        'form':create_course_form
     })
