9ff32e555794a21812e7c4c946a075abd02a0964
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-09 13:20:58 +0000

    created templates folder in catalog and modified views file in catalog folder

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
+def show_courses(request):
+    all_courses = Course.object.fliter()
+    return render(request, 'catalog/courses.template.html', {
+        'all_courses':all_courses
+    })
