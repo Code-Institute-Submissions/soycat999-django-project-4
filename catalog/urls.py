d25195b73dc7de96ac6b87042c5c5ff1aeb6c218
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-09 15:29:36 +0000

    created a form and added a route

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,5 @@ from .views import show_courses, create_course
 
 urlpatterns = [
     path('', show_courses),
-    path('create', create_course)
+    path('create', create_course),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -15,13 +15,12 @@ def show_courses(request):
 def create_course(request):
     if request.method == 'POST':
         create_course_form = CourseForm(request.POST)
-        if create_course_form.is_valid():
+        if create_course_form.is_valid():  
             create_course_form.save()
             return redirect(reverse(show_courses))
        
     else:
         create_course_form = CourseForm()
-    
     
     return render(request, 'catalog/create_course.template.html', {
         'form':create_course_form
