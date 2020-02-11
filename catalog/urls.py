f00717664fedb8fd0febdc20a43114633ff262ea
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-11 15:23:33 +0000

    included an update route to update the catalog and created a new template for delete function

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 # .views refer to the views.py in the current directory as this file
 
-from .views import show_courses, create_course 
+from .views import show_courses, create_course, update_course
 
 urlpatterns = [
-    path('', show_courses),
+    path('', show_courses, name='show_courses'),
     path('create', create_course),
+    path('update/<course_id>', update_course, name='update_course'),
 ]
