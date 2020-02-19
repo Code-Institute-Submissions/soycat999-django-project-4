4399f6f738eee1742b091307d2006aa6d9d4710e
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-19 15:05:09 +0000

    registed models, changed Course to games

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Course, Instructor
+from .models import Games, Category
 
-admin.site.register(Course)
-admin.site.register(Instructor)
+admin.site.register(Games)
+admin.site.register(Category)
