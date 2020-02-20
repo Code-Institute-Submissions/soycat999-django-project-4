c6fcc8cf3ce3f933f0204a2fa9d69ae1dd33ba54
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-20 13:43:05 +0000

    created css files and modified index.template.html to include a navbar

--- ProjectPixie/settings.py
+++ ProjectPixie/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'users',
     'cart',
-    
+
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -125,7 +125,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
 
 STATIC_URL = '/static/'
 
