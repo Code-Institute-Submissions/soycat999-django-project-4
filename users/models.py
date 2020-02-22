99b07ab9120fc7d1327e3387f8a131d21d388ef0
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-22 13:50:05 +0000

    added users class and model field reference

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Users(models.Model):
+    Username = models.CharField(blank=False, max_length=100)
+    Rating = models.IntegerField(max_digits=10)
+    Reviews = models.CharField(blank=False, max_length=255)
