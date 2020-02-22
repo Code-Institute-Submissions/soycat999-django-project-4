b9d3f0b9a11bf94c3239a7acdc04b3541ab0f7f3
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-22 12:35:47 +0000

    modified file

--- cart/views.py
+++ cart/views.py
@@ -1,29 +1,29 @@
 from django.shortcuts import render,  get_object_or_404, redirect
 from django.contrib import messages
-from catalog.models import Course
+from catalog.models import Games
 # Create your views here.
 
 
-def add_to_cart(request, course_id):
+def add_to_cart(request, games_id):
     # attempt to get existing cart from the session using the key "shopping_cart"
     # the second argument will be the default value if 
     # if the key does not exist in the session
     cart = request.session.get('shopping_cart', {})
     
     # we check if the course_id is not in the cart. If so, we will add it
-    if course_id not in cart:
-        course = get_object_or_404(Course, pk=course_id)
+    if games_id not in cart:
+        games = get_object_or_404(Games, pk=games_id)
         # course is found, let's add it to the cart
-        cart[course_id] = {
-            'id':course_id,
-            'title': course.title,
+        cart[games_id] = {
+            'id':games_id,
+            'title': games.title,
             'cost': 99
             }
         
         # save the cart back to sessions
         request.session['shopping_cart'] = cart
         
-        messages.success(request, "Course has been added to your cart!")
+        messages.success(request, "Games has been added to your cart!")
         return redirect('/catalog/')
     else:
         return redirect('/catalog/')
