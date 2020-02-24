475f95c44c3324e68ef466793231c8927158a200
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-24 22:15:52 +0000

    modified urls.py file, added path for reviews

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import signup, profile
+from .views import signup, profile, reviews
 
 
 urlpatterns = [
     path('signup', signup ),
     path('profile', profile),
+    path('reviews', reviews),
   
 ]
