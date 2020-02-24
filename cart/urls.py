d84955420ceabb7b7dc372e8823bfb66ca6be6eb
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-24 12:09:47 +0000

    change courses id to games id

--- cart/urls.py
+++ cart/urls.py
@@ -4,6 +4,6 @@ from .views import add_to_cart
 
 urlpatterns = [
     
-    path('cart/add/<course_id>', add_to_cart, name='add_to_cart')
+    path('cart/add/<games_id>', add_to_cart, name='add_to_cart')
   
 ]
