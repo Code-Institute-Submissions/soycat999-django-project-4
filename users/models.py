636c69ce2bef23e68329d08a7a08e03d4720959f
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-24 12:01:02 +0000

    modified models, changed users to reviews

--- users/models.py
+++ users/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from datetime import datetime
 
 
 
 # Create your models here.
 
-class Users(models.Model):
+class Reviews(models.Model):
     Username = models.CharField(blank=False, max_length=100)
     Rating = models.DecimalField(max_digits=10, decimal_places=0)
-    Reviews = models.CharField(blank=False, max_length=255)
+    Reviews = models.TextField(blank=False)
