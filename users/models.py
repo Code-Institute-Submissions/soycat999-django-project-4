116fa2572585e174c25eb1884bf2b5de4d070965
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-24 22:13:10 +0000

    created a path for reviews

--- users/models.py
+++ users/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from pyuploadcare.dj.models import ImageField
+
 
 
 
@@ -6,5 +8,13 @@ from django.db import models
 
 class Reviews(models.Model):
     Username = models.CharField(blank=False, max_length=100)
+    Title = models.TextField(blank=False, max_length=255)
     Rating = models.DecimalField(max_digits=10, decimal_places=0)
     Reviews = models.TextField(blank=False)
+    Image = ImageField(null=True)
+
+
+    def __str__(self):
+        return self.Username
+    class Meta:
+        verbose_name_plural = "Reviews"
