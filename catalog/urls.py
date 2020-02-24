ec5ba972d2a971497d3ba83b4d9ed7a0039fa1eb
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-24 12:16:13 +0000

    modified file

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 # .views refer to the views.py in the current directory as this file
 
-from .views import show_games, create_games, update_games, confirm_delete_games, actually_delete_games
+from .views import show_games,show_category, create_games, update_games, confirm_delete_games, actually_delete_games
 
 urlpatterns = [
     path('', show_games, name='show_games'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('update/<games_id>', update_games, name='update_games'),
     path('confirm_delete/<games_id>', confirm_delete_games),
     path('actually_delete/<games_id>', actually_delete_games, name='delete_games'),
+    path('category', show_category, name='show_category'),
 ]
