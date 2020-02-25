8195ab2d9e1e9debfa479eea00d4504a2899a18e
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-25 11:05:24 +0000

    modified user models

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from pyuploadcare.dj.models import ImageField
+from datetime import datetime
+
 
 
 
@@ -7,14 +8,16 @@ from pyuploadcare.dj.models import ImageField
 # Create your models here.
 
 class Reviews(models.Model):
-    Username = models.CharField(blank=False, max_length=100)
-    Title = models.TextField(blank=False, max_length=255)
-    Rating = models.DecimalField(max_digits=10, decimal_places=0)
-    Reviews = models.TextField(blank=False)
-    Image = ImageField(null=True)
+    username = models.CharField(blank=False, max_length=100)
+    game_title = models.CharField(blank=False, max_length=50) 
+    # -- need to have a dropdown menu for title of games
+    rating = models.DecimalField(null=True, max_digits=10, decimal_places=0)
+    # need to limit to ten
+    reviews = models.TextField(blank=False)
+    posted_on = models.DateTimeField(default=datetime.now, blank=True)
 
 
     def __str__(self):
-        return self.Username
+        return self.username
     class Meta:
         verbose_name_plural = "Reviews"
