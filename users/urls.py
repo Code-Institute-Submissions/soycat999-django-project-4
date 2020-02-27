f1afa96727f85efd516b3744dbeb288c2f089e5f
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-27 04:46:00 +0000

    created path for creating reviews

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import signup, profile, reviews
+from .views import signup, profile, reviews, create_reviews
 
 
 urlpatterns = [
     path('signup', signup ),
     path('profile', profile),
     path('reviews', reviews),
+    path('create_reviews', create_reviews),
 
 ]
