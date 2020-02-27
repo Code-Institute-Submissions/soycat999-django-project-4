840abccbbcd3f38f44fa78649262dbfaf948737d
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-27 18:41:09 +0000

    created path for creating and deleting reviews

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import signup, profile, reviews, create_reviews
+from .views import signup, profile, reviews, create_reviews,delete_reviews,actually_delete_reviews
 
 
 urlpatterns = [
     path('signup', signup ),
     path('profile', profile),
-    path('reviews', reviews),
+    path('reviews', reviews,name='reviews'),
+    path('delete_reviews/<reviews_id>',delete_reviews,name='delete_reviews'),
+    path('actually_delete_reviews/<reviews_id>',actually_delete_reviews,name='actually_delete_reviews'),
     path('create_reviews', create_reviews),
 
 ]
