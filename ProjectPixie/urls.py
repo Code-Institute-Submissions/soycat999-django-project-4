1ce68b638151df4c7734a5e2e806b90ecde9d2e1
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-27 16:53:41 +0000

    modified file

--- ProjectPixie/urls.py
+++ ProjectPixie/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('home/', include('home.urls')),
     path('catalog/', include('catalog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('cart/', include('cart.urls'))
 ]
 
