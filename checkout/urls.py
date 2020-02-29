d86d6e5b7fd4367089c2a4c5165f05472648f734
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-29 13:51:18 +0000

    created and modified checkout.template.html

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import checkout, checkout_success, checkout_cancelled, payment_completed
+from .views import checkout, checkout_success, checkout_cancelled
 
 urlpatterns = [
-    path('', checkout, name='checkout' ),
+    path('', checkout, name='checkout'),
     path('success', checkout_success),
-    path('cancelled', checkout_cancelled),
-    path('payment_completed', payment_completed),
+    path('cancelled', checkout_cancelled),    
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, reverse, HttpResponse, get_object_or_404
-from django.conf import settings
-import stripe
-from catalog.models import Games
-from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+from django.shortcuts import render, reverse, HttpResponse, get_object_or_404, redirect
 
+#import settings so that we can access the public stripe key
+from django.conf import settings
+from django.contrib import messages
+import stripe
+
+from catalog.models import Games
+
+# Create your views here.
 def checkout(request):
     stripe.api_key = settings.STRIPE_SECRET_KEY
     
@@ -15,13 +19,13 @@ def checkout(request):
     line_items = []
     
     # generate the line_items
-    for id, games in cart.items():
+    for id,games in cart.items():
         # For each item in the cart, get its details from the database
         games = get_object_or_404(Games, pk=id)
         line_items.append({
             'name': games.title,
             'amount': int(games.cost*100), #convert to cents, in integer
-            'currency':'sgd',
+            'currency':'usd',
             'quantity':1
         })
     
@@ -34,42 +38,16 @@ def checkout(request):
     )
     
     # render the template
-    return render(request, 'checkout/checkout.template.html', {
+    return render(request, 'checkout/checkout.html', {
         'session_id':session.id,
         'public_key': settings.STRIPE_PUBLISHABLE_KEY
     })
     
 def checkout_success(request):
-    return HttpResponse("Checkout success")
+    messages.success(request, "Checkout Success")
+    request.session['shopping_cart'] = {}
+    return redirect('home')
     
 def checkout_cancelled(request):
-    return HttpResponse("Checkout cancelled")
-
-@csrf_exempt
-def payment_completed(request):
-  payload = request.body
-  sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-  event = None
-
-  try:
-    event = stripe.Webhook.construct_event(
-      payload, sig_header, endpoint_secret
-    )
-  except ValueError as e:
-    # Invalid payload
-    return HttpResponse(status=400)
-  except stripe.error.SignatureVerificationError as e:
-    # Invalid signature
-    return HttpResponse(status=400)
-
-  # Handle the checkout.session.completed event
-  if event['type'] == 'checkout.session.completed':
-    session = event['data']['object']
-
-    # Fulfill the purchase...
-    handle_checkout_session(session)
-
-  return HttpResponse(status=200)
-  
-def handle_checkout_session(session):
-     print(session)
+    messages.success(request, "Checkout Cancelled")
+    return redirect('view_cart')
