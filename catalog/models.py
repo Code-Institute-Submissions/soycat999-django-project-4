44c6d65582b6ab8c9d2149cd44e8b6ebc85a6835
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-02-29 14:22:37 +0000

    modified file, deleted tags and changed ImageField to models.ImageField

--- catalog/models.py
+++ catalog/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from pyuploadcare.dj.models import ImageField
 from datetime import datetime
 
 # Create your models here.
 class Category(models.Model):
     Genre = models.CharField(blank=False, max_length=100)
-    Tags = models.CharField(blank=False, max_length=100)
-    
+
     def __str__(self):
         return self.Genre
     class Meta:
@@ -20,7 +18,7 @@ class Games(models.Model):
     release_date = models.DateTimeField(default=datetime.now, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     
-    image = ImageField(blank=True, null=True)
+    image = models.ImageField(blank=True)
     cost = models.FloatField(blank=False)
 
    
