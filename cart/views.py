3cd35beadf55e0b39990de563136d2347e0bef02
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2020-03-01 15:45:03 +0000

    added cart subtotal, and qty for users to add or reduce qty

--- cart/views.py
+++ cart/views.py
@@ -18,14 +18,16 @@ def add_to_cart(request, games_id):
         cart[games_id] = {
             'id':games_id,
             'title': games.title,
-            'cost': 99
+            'cost': round(float(games.price),2),
+            'qty':1,
+            'subtotal' : round(float(games.price),2),
             }
         
         # save the cart back to sessions
         request.session['shopping_cart'] = cart
-        messages.success(request, "You have added a new game!")
+        messages.success(request, "You have added a new game into cart!")
     else:
-        messages.success(request, "The course is already in your shopping cart")
+        messages.success(request, "The game is already in your shopping cart")
     return redirect('/catalog/')
         
 def view_cart(request):
@@ -47,3 +49,40 @@ def remove_from_cart(request, course_id):
         messages.success(request, "Item removed from cart successfully!")
         
     return redirect('/catalog/')
+    
+def add_qty(request, cart_id):
+    # retrieve the cart from session
+    cart = request.session.get('shopping_cart',{})
+    
+    # if the course is in the cart
+    if cart_id in cart:
+        # remove it from the cart
+        if 'qty' not in cart[cart_id]:
+            cart[cart_id]['qty'] = 1
+            cart[cart_id]['subtotal'] = round(float(cart[cart_id]['cost']),2),
+        else:
+            cart[cart_id]['qty'] += 1
+            cart[cart_id]['subtotal'] = round(cart[cart_id]['qty'] * float(cart[cart_id]['cost']),2)
+        # save back to the session
+        request.session['shopping_cart'] = cart
+        
+    return redirect('/cart/')
+    
+def remove_qty(request, cart_id):
+    # retrieve the cart from session
+    cart = request.session.get('shopping_cart',{})
+    
+    # if the course is in the cart
+    if cart_id in cart:
+        # remove it from the cart
+        if 'qty' not in cart[cart_id]:
+            cart[cart_id]['qty'] = 0
+        else:
+            cart[cart_id]['qty'] -= 1
+            cart[cart_id]['subtotal'] = round(cart[cart_id]['qty'] * float(cart[cart_id]['cost']),2)
+        # save back to the session
+        if cart[cart_id]['qty']<=0:
+            del cart[cart_id]
+        request.session['shopping_cart'] = cart
+        
+    return redirect('/cart/')
